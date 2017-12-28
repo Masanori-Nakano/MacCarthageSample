8092d07f64652be5f4527a075029b0dfcb13908e
中野 雅章 <tzr250rs_94s@yahoo.co.jp>
2017-12-28 22:36:06 +0900

    プロジェクト追加

---
+++ MacCarthageSample/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MacCarthageSample
+//
+//  Created by 電気なまず on 2017/12/28.
+//  Copyright © 2017年 Masanori Nakano. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
